49603eb8e438d6958fb726b755a7485f0176a39e
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-06-02 20:46:38 -0300

    :recycle: refactor: using AutoMigrate function to migrate tables.

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 
+	"github.com/azevedoguigo/projeto-odonto-back.git/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,9 +12,14 @@ var DB *gorm.DB
 
 func InitDB() {
 	dsn := "host=localhost user=postgres password=postgres dbname=projeto_odonto port=5432 sslmode=disable TimeZone=America/Maceio"
+
 	var err error
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
+
+	DB.AutoMigrate(&models.User{}, &models.Dentist{}, &models.Patient{}, &models.Service{})
 }
--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"github.com/azevedoguigo/projeto-odonto-back.git/config"
-	"github.com/azevedoguigo/projeto-odonto-back.git/models"
 	"github.com/azevedoguigo/projeto-odonto-back.git/routes"
 )
 
 func main() {
 	config.InitDB()
-	models.MigrateUserTable(config.DB)
-	models.MigratePatientTable(config.DB)
-	models.MigrateDentistTable(config.DB)
 
 	router := routes.SetupRouter()
 	router.Run()
